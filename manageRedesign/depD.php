90723a7fe6d05ba6a265b0dc97c63bb7cfd45061
Rasan-hue <62095526+Rasan-hue@users.noreply.github.com>
2020-05-15 15:17:22 +0300

    Data tables!

--- manageRedesign/depD.php
+++ manageRedesign/depD.php
@@ -1,98 +1,123 @@
-<?php
+<?php include('header.php'); ?>
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
 
- session_start();
-include('./MyDB.php');
-$Error = "";
-?>
+          <!-- Page Heading -->
+          <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
 
-<html>
-<body>
-
-
-  </head>
-
-  <body bgcolor="#E6E6FA">
-
-  <header class="header-two-bars">
-  	<div class="header-first-bar">
-  		<div class="header-limiter">
-  			<h1><a href="">StudentSystem <span> Admin <span> </span></a></h1>
-<h1>	<a href="admin.php"><li>Home </span></li></a></h1>
-
-
-
-
-
-  		</div>
-  	</div>
-  </header>
-
-
-  <style>
-    #A{
-      background-color: #c0c0c0;
-      color: black;
-      padding: 15px 15px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-    }
-  </style>
-
-
-       <link rel="stylesheet" href="Style.css">
-     <link rel="stylesheet" type="text/css" href="Pages_CSS.css">
-     <link rel="stylesheet" href="second-bar.css">
-     <link rel="stylesheet" href="header-second-bar.css">
-     <link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
-     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
-     </head>
-
-
-       	<div class="form-style-5">
-
-
-                 <a href="#navigation"><div id="center">^</div></a>
-                 <div id="content" STYLE="TEXT-ALIGN:CENTER;">
-
-                    <div class="form-style-22">
-
-                      <Table border="7">
-                        <Tr>
-                          <Th>Department Name</Th>
-                        </Tr>
-                        <tr>
-                          <td>
-                            <?php
-                              $result = $MyDB->query("SELECT * FROM department");
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Department Name</th>
+                      <!--  <th>Username</th>
+                      <th>User Role</th>
+                      <th>Password</th>
+                     <th>Start date</th>
+                      <th>Salary</th> -->
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    <tr>
+                      <th>Department Name</th>
+                     <!--  <th>Username</th>
+                      <th>User Role</th>
+                      <th>Password</th>
+                      <th>Start date</th>
+                      <th>Salary</th> -->
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                  <?php
+                  $result = $MyDB->query("SELECT * FROM department");
                               while($row = $result->fetch_assoc()){
                                 $name = $row['Dep_Name'];
-                                echo $name."<br />";
-                                }
-                            ?>
-                          </td>
-                        </tr>
-                        <?php
-                        /*
-                        include('MyDB.php');
-                        	error_reporting(E_ERROR | E_PARSE);
-                        	$result = $MyDB->query("SELECT Dep_ID,Dep_Name,F_ID FROM department where F_ID='$faculty' ");
-                        	while($row = $result->fetch_assoc()){
-                        		$ID = $row['Dep_ID'];
-                        		$name = $row['Dep_Name'];
-                            $FID = $row['F_ID'];
-                            }
-                            */
-                        ?>
+                                echo "<tr><td>".$name."</td></tr>";
+                              } 
+                ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
 
-                    </Table>
-                    </div>
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php include('footer.php'); ?>
+
+  <!-- Page level plugins -->
+  <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/datatables-demo.js"></script>
 
 
 
 
 
-      </form>
 
-   </body>
-</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
--- manageRedesign/showUser_page.php
+++ manageRedesign/showUser_page.php
@@ -1,46 +1,112 @@
 <?php include('header.php'); ?>
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
 
-    <a href="#navigation">
-      <div id="center">^</div>
-    </a>
-    <div id="content" STYLE="TEXT-ALIGN:CENTER;">
-      <div class="form-style-22">
-        <Table border="7">
-          <Tr>
-            <Th>User_ID</Th>
-            <Th>User_Name</Th>
-            <Th>User_Role</Th>
-            <Th>User_Pass</Th>
-            <Th>UPDATE</Th>
+          <!-- Page Heading -->
+          <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
 
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>User ID</th>
+                      <th>Username</th>
+                      <th>User Role</th>
+                      <th>Password</th>
+                      <!-- <th>Start date</th>
+                      <th>Salary</th> -->
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    <tr>
+                      <th>User ID</th>
+                      <th>Username</th>
+                      <th>User Role</th>
+                      <th>Password</th>
+                      <!-- <th>Start date</th>
+                      <th>Salary</th> -->
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                  <?php
+                        $b = $MyDB->query("SELECT * FROM user");
+                        while ($ex = $b->fetch_assoc()) {
 
-          </Tr>
-          <?php
-          $b = $MyDB->query("SELECT * FROM user");
-          while ($ex = $b->fetch_assoc()) {
+                        ?>
+                            <Tr>
+                            <Td> <?php echo $ex['User_ID']; ?></Td>
+                            <Td><?php echo $ex['User_Name']; ?></Td>
+                            <Td><?php echo $ex['User_Role']; ?></Td>
+                            <Td> <?php echo $ex['User_Pass']; ?></Td>
+                            </Tr>
 
-          ?>
-            <Tr>
-              <Td> <?php echo $ex['User_ID']; ?></Td>
-              <Td><?php echo $ex['User_Name']; ?></Td>
-              <Td><?php echo $ex['User_Role']; ?></Td>
-              <Td> <?php echo $ex['User_Pass']; ?></Td>
-              <Td>
-                <a HREF="delUser.php?ID= <?php echo $ex['User_ID']; ?>
-                <?php echo $ex['User_Name']; ?>
-                <?php echo $ex['User_Role']; ?>
-                <?php echo $ex['User_Pass']; ?>">
-                </a>
-              </Td>
-              <Td><a HREF="Update.php?ID= <?php echo $ex['User_ID']; ?>">   
-             </Tr>
+                    <?php } ?>
 
-          <?php } ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
 
-        </Table>
+        </div>
+        <!-- /.container-fluid -->
 
+      </div>
+      <!-- End of Main Content -->
 
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
 
+    </div>
+    <!-- End of Content Wrapper -->
 
-    }
-    <?php include('footer.php'); ?>
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php include('footer.php'); ?>
+
+  <!-- Page level plugins -->
+  <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/datatables-demo.js"></script>
+
